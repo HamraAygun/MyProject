2cd5faccf514ae83aa01a3111e8c4e8e239688cc
HamraAygun <121159398+HamraAygun@users.noreply.github.com>
2022-12-25 14:54:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+public class Program
+
+{
+    public static void Main(string[] args)
+
+    // Ölçüsü verilen dikdörtgenin alanını ve çevresini hesaplayan kodu yazınız..
+    {
+        int a, b, c, d;
+
+        Console.WriteLine("Dikdörtgenin bir kenar ölçüsünü giriniz." + "\n");
+
+        a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("Dikdörtgenin diğer kenar ölçüsünü giriniz." + "\n");
+
+        b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(" ");
+
+        c = a * b;
+
+        Console.WriteLine("Dikdörtgenin alanı :" + c + "\n");
+
+        d = 2 * (a + b);
+
+        Console.WriteLine("Dikdörtgenin Çevresi :" + d + "\n");
+
+        Console.ReadKey();
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
